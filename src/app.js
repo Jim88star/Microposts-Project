a4f3d5e1dfee953e4f77173b5c77112137b883a9
Jim Gardner <jim88code@gmail.com>
2018-11-12 20:14:27 -0600

    Get & Display Posts

--- src/app.js
+++ src/app.js
@@ -11,3 +11,16 @@ import greeting from './mymodule2';
 // console.log(mod.sayHello());
 
 console.log(greeting);
+
+import { http } from './http';
+import { ui } from './ui';
+
+// Get posts on DOM load
+document.addEventListener('DOMContentLoaded', getPosts);
+
+function getPosts() {
+  http.get('http://localhost:3000/posts')
+    // .then(data => console.log(data))
+    .then(data => ui.showPosts(data))
+    .catch(err => console.log(err));
+}
